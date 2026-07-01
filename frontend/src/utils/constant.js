d3c3aea5293c7ae9471fe87182187ed713610df6
Ayush <ayushkumar34966@gmail.com>
2026-07-01 22:17:20 +0530

    Fix CORS for Vercel

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL="http://localhost:3000/api"
-export const SOCKET_URL = "http://localhost:3000"
+export const BASE_URL = import.meta.env.VITE_BASE_URL + "/api";
+export const SOCKET_URL = import.meta.env.VITE_BASE_URL;
